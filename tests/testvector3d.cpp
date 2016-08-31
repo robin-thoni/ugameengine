d4b277472acc0a72094a6c30221d90525144cc9c
Robin THONI <robin@rthoni.com>
2016-08-31 18:13:13 -0400

    vector tests; removed binary files; gitignore

--- tests/testvector3d.cpp
+++ tests/testvector3d.cpp
@@ -8,6 +8,9 @@ class TestVector3D : public QObject
 private slots:
     void gettersSetters1();
     void gettersSetters2();
+    void gettersSetters3();
+    void mult();
+    void div();
 };
 
 void TestVector3D::gettersSetters1()
@@ -20,10 +23,41 @@ void TestVector3D::gettersSetters1()
 
 void TestVector3D::gettersSetters2()
 {
-    Vector3D v(1.42, 2.0, 3.0);
-    QCOMPARE(v.getX(), 1.42);
-    QCOMPARE(v.getY(), 2.0);
-    QCOMPARE(v.getZ(), 3.0);
+    double x = 1.42, y = 2.0, z = 3.0;
+    Vector3D v(x, y, z);
+    QCOMPARE(v.getX(), x);
+    QCOMPARE(v.getY(), y);
+    QCOMPARE(v.getZ(), z);
+}
+
+void TestVector3D::gettersSetters3()
+{
+    double x = 1.42, y = 2.0, z = 3.0;
+    Vector3D v;
+    v.setX(x).setY(y).setZ(z);
+    QCOMPARE(v.getX(), x);
+    QCOMPARE(v.getY(), y);
+    QCOMPARE(v.getZ(), z);
+}
+
+void TestVector3D::mult()
+{
+    double x = 1.42, y = 2.0, z = 3.0, m = 2.0;
+    Vector3D v(x, y, z);
+    v.mult(m);
+    QCOMPARE(v.getX(), x * m);
+    QCOMPARE(v.getY(), y * m);
+    QCOMPARE(v.getZ(), z * m);
+}
+
+void TestVector3D::div()
+{
+    double x = 1.42, y = 2.0, z = 3.0, m = 2.0;
+    Vector3D v(x, y, z);
+    v.div(m);
+    QCOMPARE(v.getX(), x / m);
+    QCOMPARE(v.getY(), y / m);
+    QCOMPARE(v.getZ(), z / m);
 }
 
 QTEST_MAIN(TestVector3D)
